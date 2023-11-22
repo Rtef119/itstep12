72267ceecd444cfa7661008ba1001dc78647cb10
rtef119 <rtef119@gmail.com>
2023-11-22 16:18:06 +0100

    Finnal_Prodget

---
+++ main.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import requests
+import sqlite3 as sq
+
+url = ("https://www.bbc.com/ukrainian/features-66330880")
+
+cn = sq.connect("base.sl3")
+cur = cn.cursor()
+
+
+response = requests.get(url)
+if response.status_code==200:
+    bs = BeautifulSoup(response.text, features='html.parser')
+    list = bs.find_all('h2')
+    for elem in list[:10]:
+        title=elem.text
+        print(title)
+
