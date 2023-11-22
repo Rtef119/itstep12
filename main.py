aece03a6e39814710c8b752c55f3420504598099
rtef119 <rtef119@gmail.com>
2023-11-22 16:35:20 +0100

    Finnal_Prodget

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ import sqlite3 as sq
 
 url = ("https://www.bbc.com/ukrainian/features-66330880")
 
-cn = sq.connect("base.sl3")
+cn = sq.connect("database.sl3")
 cur = cn.cursor()
 
+cur = sq.connect("database.sl3")
+
+
+cn.commit()
+
+cn.close()
 
 response = requests.get(url)
 if response.status_code==200:
@@ -15,4 +21,5 @@ if response.status_code==200:
     for elem in list[:10]:
         title=elem.text
         print(title)
+
 
